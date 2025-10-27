2c4b6092b3bec8625ea3cce2684d97298bc53005
Samael Morningstar <samael.morningstart@protonmail.me>
2025-10-27 23:04:40 +1000

    db loading with sqlite, i give up on relation linking with fkeys and pkeys

--- db/schema.sql
+++ db/schema.sql
@@ -1,101 +1,98 @@
-CREATE TABLE IF NOT EXISTS agency(
-    id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    url TEXT NOT NULL,
-    lang TEXT,
-    phone TEXT,
-    fare_url TEXT,
-    email TEXT,
-    cemv_support INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS stops(
-    id TEXT PRIMARY KEY,
-    code TEXT,
-    name TEXT NOT NULL,
-    tts_stop_name TEXT,
-    desc TEXT,
-    lat REAL NOT NULL,
-    lon REAL NOT NULL,
-    zone_id TEXT,
-    url TEXT,
-    location_type INTEGER NOT NULL,
-    parent_station INTEGER,
-    FOREIGN KEY(parent_station) REFERENCES stops(id)
-);
-
-CREATE TABLE IF NOT EXISTS routes(
-    id TEXT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `agency`(
     agency_id TEXT,
-    short_name TEXT NOT NULL,
-    long_name TEXT NOT NULL,
-    desc TEXT,
-    type INTEGER NOT NULL ON CONFLICT REPLACE DEFAULT 0,
-    color TEXT,
-    text_color TEXT,
-    sort_order INTEGER,
-    continuous_pickup INTEGER,
-    continuous_drop_off INTEGER,
-    network_id TEXT,
-    cemv_support INTEGER,
-    FOREIGN KEY(agency_id) REFERENCES agency(id)
+    agency_name TEXT,
+    agency_url TEXT,
+    agency_timezone TEXT,
+    agency_lang TEXT,
+    agency_phone TEXT
 );
 
-CREATE TABLE IF NOT EXISTS calendar(
-    service_id TEXT PRIMARY KEY,
-    monday INTEGER NOT NULL,
-    tuesday INTEGER NOT NULL,
-    wednesday INTEGER NOT NULL,
-    thursday INTEGER NOT NULL,
-    friday INTEGER NOT NULL,
-    saturday INTEGER NOT NULL,
-    sunday INTEGER NOT NULL,
-    start_date REAL NOT NULL,
-    end_date REAL NOT NULL
+CREATE TABLE IF NOT EXISTS `calendar_dates`(
+    service_id TEXT,
+    date TEXT,
+    exception_type TEXT
 );
 
-CREATE TABLE IF NOT EXISTS calendar_dates(
-    service_id TEXT NOT NULL,
-    date REAL NOT NULL,
-    exception_type INTEGER NOT NULL,
-    FOREIGN KEY(service_id) REFERENCES calendar(service_id)
-    PRIMARY KEY(service_id, date)
+CREATE TABLE IF NOT EXISTS `calendar`(
+    service_id TEXT,
+    monday TEXT,
+    tuesday TEXT,
+    wednesday TEXT,
+    thursday TEXT,
+    friday TEXT,
+    saturday TEXT,
+    sunday TEXT,
+    start_date TEXT,
+    end_date TEXT
 );
 
-CREATE TABLE IF NOT EXISTS shapes(
-    id TEXT NOT NULL,
-    pt_sequence INTEGER NOT NULL,
-    pt_lat REAL NOT NULL,
-    pt_lon REAL NOT NULL,
-    PRIMARY KEY(id, pt_sequence)
+CREATE TABLE IF NOT EXISTS `feed_info`(
+    feed_publisher_name TEXT,
+    feed_publisher_url TEXT,
+    feed_lang TEXT,
+    feed_start_date TEXT,
+    feed_end_date TEXT
 );
 
-CREATE TABLE IF NOT EXISTS trips(
-    id TEXT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `routes`(
+    route_id TEXT,
+    route_short_name TEXT,
+    route_long_name TEXT,
+    route_desc TEXT,
+    route_type TEXT,
+    route_url TEXT,
+    route_color TEXT,
+    route_text_color TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `stops`(
+    stop_id TEXT,
+    stop_code TEXT,
+    stop_name TEXT,
+    stop_desc TEXT,
+    stop_lat TEXT,
+    stop_lon TEXT,
+    zone_id TEXT,
+    stop_url TEXT,
+    location_type TEXT,
+    parent_station TEXT,
+    platform_code TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `stop_times`(
+    trip_id TEXT,
+    arrival_time TEXT,
+    departure_time TEXT,
+    stop_id TEXT,
+    stop_sequence TEXT,
+    pickup_type TEXT,
+    drop_off_type TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `shapes`(
+    shape_id TEXT,
+    shape_pt_lat TEXT,
+    shape_pt_lon TEXT,
+    shape_pt_sequence TEXT
+);
+
+CREATE TABLE IF NOT EXISTS `trips`(
     route_id TEXT,
     service_id TEXT,
-    headsign TEXT,
-    short_name TEXT,
-    direction_id INTEGER,
+    trip_id TEXT,
+    trip_headsign TEXT,
+    direction_id TEXT,
     block_id TEXT,
-    shape_id INTEGER,
-    wheelchair_accessible INTEGER,
-    bikes_allowed INTEGER,
-    cars_allowed INTEGER,
-    FOREIGN KEY(shape_id) REFERENCES shapes(id),
-    FOREIGN KEY(service_id) REFERENCES calendar(service_id),
-    FOREIGN KEY(route_id) REFERENCES routes(id)
+    shape_id TEXT
 );
 
-CREATE TABLE IF NOT EXISTS stop_times(
-    trip_id TEXT NOT NULL,
-    stop_sequence INTEGER NOT NULL,
-    arrival_time REAL,
-    departure_time REAL,
-    stop_id TEXT,
-    pickup_type INTEGER,
-    drop_off_type INTEGER,
-    FOREIGN KEY(stop_id) REFERENCES stops(id),
-    FOREIGN KEY(trip_id) REFERENCES trips(id),
-    PRIMARY KEY(trip_id, stop_sequence)
-);
+.mode csv
+
+.import '../data/agency.txt' agency;
+.import '../data/calendar_dates.txt' calendar_dates;
+.import '../data/feed_info.txt' feed_info;
+.import '../data/routes.txt' routes;
+.import '../data/shapes.txt' shapes;
+.import '../data/stop_times.txt' stop_times;
+.import '../data/stops.txt' stops;
+.import '../data/trips.txt' trips;
