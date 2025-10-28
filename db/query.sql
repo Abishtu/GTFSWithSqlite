0e0946217a7322b651ef7ae8a42c19ccd1b98fd9
Harikesha Suresh <harikeshasuresh@gmail.com>
2025-10-28 20:48:56 +1000

    (#1) Primary key and Foreign Key from csv imports

---
+++ db/query.sql
@@ -0,0 +1,8 @@
+SELECT route_id, route_short_name, route_long_name FROM routes
+WHERE route_id IN (
+    SELECT route_id FROM trips
+    WHERE trip_id IN (
+        SELECT trip_id FROM stop_times
+        WHERE stop_id = '10'
+    )
+)
