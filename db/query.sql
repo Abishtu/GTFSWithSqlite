4c461e161070bff0fe920e6d2e76b3b0807b1b03
Samael Morningstar <samael.morningstart@protonmail.me>
2025-10-29 07:42:58 +1000

    db made, have a little sample query

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,5 @@
 SELECT route_id, route_short_name, route_long_name FROM routes
-WHERE route_id IN (
+WHERE DISTINCT route_id IN (
     SELECT route_id FROM trips
     WHERE trip_id IN (
         SELECT trip_id FROM stop_times
